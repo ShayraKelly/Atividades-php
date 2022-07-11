b0f10e6936d9793c008666e7587c45b15e6e00a1
ShayraKelly <108482817+ShayraKelly@users.noreply.github.com>
2022-07-10 21:22:41 -0300

    02

--- Venda.php
+++ Venda.php
@@ -1,39 +1,85 @@
 <?php
+
 class Venda{
     public $produto;
-    public $valor;
+    public $soma_valor;
+    public $venda;
 
-    public function calcValor(){
-        for($x = 1; $x <= 2; $x++){
-            $this->valor = $this->valor + $this->produto[$x]->Calcular();
+    public function getvalor_calc(){
+        for($i = 1; $i <= 4; $i++){
+            $this->soma_valor = $this->soma_valor + $this->produto[$i]->getCalc();
         }
-        echo "Valor Total: " . $this->valor;
+        return $this->soma_valor;
     }
 
-    public function Imprimir(){
-        for($y = 1; $y <= 2; $y++){
-            echo "Produtos <br><br> Código do produto: ";
-            echo $this->produto[$y]->id . "<br>Produto: ";
-            echo $this->produto[$y]->nome . "<br>Preço: ";
-            echo $this->produto[$y]->preco . "<br>Quantidade: ";
-            echo $this->produto[$y]->quantidade . "<br><br>";
+    public function NaoPaga(){
+        for($i = 1; $i <= 4; $i++){
+            if($this->produto[$i]->info_venda == 1){
+                echo "ID: " . $this->produto[$i]->id;
+                echo "<br>Nome: " . $this->produto[$i]->nome;
+                echo "<br>Valor: " . $this->produto[$i]->valor;
+                echo "<br>Quantidade: " . $this->produto[$i]->qtd;
+                echo "<strong><br>Venda à Prazo não paga<br></strong>";
+                echo "<br><br>";
+            }
         }
     }
 
-    public function gerarProduto(){
-        $this->produto[1] = new Produto();
-        $this->produto[1]->id = 2;
+    public function PrazoPaga(){
+        for($i = 1; $i <= 4; $i++){
+            if($this->produto[$i]->info_venda == 2){
+                echo "ID: " . $this->produto[$i]->id;
+                echo "<br>Nome: " . $this->produto[$i]->nome;
+                echo "<br>Valor: " . $this->produto[$i]->valor;
+                echo "<br>Quantidade: " . $this->produto[$i]->qtd;
+                echo "<strong><br>Venda à Prazo paga<br></strong>";
+                echo "<br><br>";
+            }
+        }
+    }
+
+    public function Paga(){
+        for($i = 1; $i <= 4; $i++){
+            if($this->produto[$i]->info_venda == 3){
+                echo "ID: " . $this->produto[$i]->id;
+                echo "<br>Nome: " . $this->produto[$i]->nome;
+                echo "<br>Valor: " . $this->produto[$i]->valor;
+                echo "<br>Quantidade: " . $this->produto[$i]->qtd;
+                echo "<strong><br>Venda paga<br></strong>";
+                echo "<br><br>";
+            }
+        }
+    }
+
+    public function gerarProdutos(){
+        $this->produto[1]= new Produtos();
+        $this->produto[1]->id = 1;
         $this->produto[1]->nome = "Sorvete";
-        $this->produto[1]->preco = 6;
-        $this->produto[1]->quantidade = 10;
+        $this->produto[1]->valor = 5;
+        $this->produto[1]->qtd = 10;
+        $this->produto[1]->info_venda = 1;
 
-        $this->produto[2] = new Produto();
-        $this->produto[2]->id = 1;
-        $this->produto[2]->nome = "Milkshake";
-        $this->produto[2]->preco = 10;
-        $this->produto[2]->quantidade = 5;
+        $this->produto[2] = new Produtos();
+        $this->produto[2]->id = 2;
+        $this->produto[2]->nome = "MilkShake";
+        $this->produto[2]->valor = 10;
+        $this->produto[2]->qtd = 6;
+        $this->produto[2]->info_venda = 2;
+
+        $this->produto[3] = new Produtos();
+        $this->produto[3]->id = 3;
+        $this->produto[3]->nome = "Picolé";
+        $this->produto[3]->valor = 3;
+        $this->produto[3]->qtd = 15;
+        $this->produto[3]->info_venda = 3;
+
+        $this->produto[4] = new Produtos();
+        $this->produto[4]->id = 4;
+        $this->produto[4]->nome = "Laranjinha";
+        $this->produto[4]->valor = 1.50;
+        $this->produto[4]->qtd = 5;
+        $this->produto[4]->info_venda = 2;
     }
-
 }
 
 ?>
